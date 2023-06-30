196d154c168c1b81a2e723a29fe6d02b390f9c4e
Gabriel Okolie <iamthatog@gmail.com>
2023-06-30 03:08:33 +0100

    Gabriel Okolie reverse linked list iamthatog@gmail.com

---
+++ linked-list/reverse_linkedlist.py
@@ -0,0 +1,25 @@
+from singly_linkedlist import SinglyLinkedList
+
+
+def reverse(nked_list):
+    if linked_list.is_empty():
+        return
+    current_node = linked_list.head_node
+    prev_node = None
+    next_node = None
+    while current_node:
+        next_node = current_node.next_node
+        current_node.next_node = prev_node
+        prev_node = current_node
+        current_node = next_node
+    linked_list.head_node = prev_node
+
+
+linked_list = SinglyLinkedList()
+
+for i in range(1, 10):
+    linked_list.insert_at_head(i)
+linked_list.print_list()
+
+linked_list.reverse()
+linked_list.print_list()
